08802ee67ce89264d38b0cd0aff26ad3312a0da7
Carlos Borrega <c.borrega@proton.me>
2024-02-04 13:43:33 +0000

    fixed section style

--- script/script.js
+++ script/script.js
@@ -3,19 +3,4 @@ function toggleMenu() {
     menuItems.classList.toggle('show');
     menuItems.classList.toggle('hide');
 }
-
-function removeScrollSnap() {
-    var container = document.querySelector('.scroller');
-  
-    // Check if the screen width is less than or equal to 480 pixels
-    if (window.innerWidth <= 480) {
-      container.classList.remove('scroller');
-    } else {
-      container.classList.add('scroller');
-    }
-  }
-  
-  // Run the function on page load and whenever the window is resized
-  document.addEventListener('DOMContentLoaded', removeScrollSnap);
-  window.addEventListener('resize', removeScrollSnap);
   
